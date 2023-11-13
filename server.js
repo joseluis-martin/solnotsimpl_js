e6c0b9d7043da68e6a6557b4e2dfd8a1667d9d67
joseluis-martin <joseluis.martin@uah.es>
2023-11-13 16:23:27 +0100

    Sin AJAX

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const https = require('https');
 const fs = require('fs');
-const axios = require('axios');
 const path = require('path');
-const multer = require('multer'); // Middleware para manejar 'multipart/form-data'
+const multer = require('multer');
+const axios = require('axios');
 
 const app = express();
 const port = 443;
 
-// Configuración para multer (para manejar la carga de archivos)
+// Configuración para multer
 const upload = multer({ dest: 'uploads/' });
 
-// Rutas estáticas para servir archivos HTML, JS, CSS, etc.
+// Servir archivos estáticos (HTML, CSS, JS, etc.)
 app.use(express.static('public'));
 
 // Ruta principal para servir la página HTML
@@ -19,14 +19,11 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-// Ruta para manejar la petición AJAX
-// 'upload.single('file')' maneja la carga del archivo XML
+// Ruta para manejar el formulario enviado
 app.post('/sendXML', upload.single('file'), async (req, res) => {
     try {
-        // El archivo XML se almacena en 'req.file'
         const xmlData = fs.readFileSync(req.file.path, 'utf8');
 
-        // Configurar opciones para la petición HTTPS
         const options = {
             method: 'POST',
             headers: { 'Content-Type': 'text/xml' },
@@ -34,23 +31,20 @@ app.post('/sendXML', upload.single('file'), async (req, res) => {
             url: 'https://test.registradores.org/xmlpeticion'
         };
 
-        // Enviar petición al Colegio de Registradores
         const response = await axios(options);
 
-        // Enviar respuesta al cliente
-        res.send(response.data);
+        // Puedes enviar una página o mensaje de respuesta aquí
+        res.send(`<pre>${response.data}</pre>`);
     } catch (error) {
         console.error(error);
         res.status(500).send('Error al procesar la petición');
     }
 });
 
-// Leer los certificados SSL
-const privateKey = fs.readFileSync('./certificados/proytasv.depeca.uah.es.pem', 'utf8');
-const certificate = fs.readFileSync('./certificados/proytasv_depeca_uah_es_cert.cer', 'utf8');
+const privateKey = fs.readFileSync('proytasv.depeca.uah.es.pem', 'utf8');
+const certificate = fs.readFileSync('proytasv_depeca_uah_es_cert.cer', 'utf8');
 const credentials = { key: privateKey, cert: certificate };
 
-// Crear servidor HTTPS
 const httpsServer = https.createServer(credentials, app);
 
 httpsServer.listen(port, () => {
